bb2fd0c27685b71b3954cb797d8b211c0e43ac95
Volodymyr <degtyarevvladimirr@gmail.com>
2025-01-23 08:14:18 +0200

    fix: css (#40)

--- src/components/ProfileListAttribute/ProfileListAttribute.tsx
+++ src/components/ProfileListAttribute/ProfileListAttribute.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 
-import { BasisBlock } from '@/shared/BasisBlock/BasisBlock.tsx';
 import { ProfileAttribute } from '@/shared/ProfileAttribute/ProfileAttribute.tsx';
 import cls from './ProfileListAttribute.module.css';
 import { getProfileAttributes } from '@/utils/db/profileAttributes.tsx';
@@ -9,14 +8,12 @@ export const ProfileListAttribute = () => {
     const [profileAttributes] = useState(getProfileAttributes());
 
     return (
-        <BasisBlock>
-            <ul data-testid='profileAttributes'>
-                {profileAttributes.map(({ icon, text, path }, index) => (
-                    <li key={index} className={cls.listItem}>
-                        <ProfileAttribute icon={icon} text={text} path={path} />
-                    </li>
-                ))}
-            </ul>
-        </BasisBlock>
+        <ul data-testid='profileAttributes' className={cls.container}>
+            {profileAttributes.map(({ icon, text, path }, index) => (
+                <li key={index} className={cls.listItem}>
+                    <ProfileAttribute icon={icon} text={text} path={path} />
+                </li>
+            ))}
+        </ul>
     );
 };
